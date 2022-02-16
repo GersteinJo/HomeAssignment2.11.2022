01b9e3d1850abd1061a6ad565ee00788494b7f89
GersteinJo <gershtein.iaa@phystech.edu>
2022-02-16 21:02:34 +0300

    All works with multimap, at least for now...

--- Task100.cpp
+++ Task100.cpp
@@ -4,7 +4,6 @@
 
 #include <iostream>
 #include <map>
-#include <unordered_map>
 #include <iterator>
 #include <experimental/random>
 #include <fstream>
@@ -12,13 +11,14 @@
 class PhoneBook
 {
 private:
-    std::map<std::string, std::string> person_Phone;
+    std::multimap<std::string, std::string> person_Phone;
 public:
     PhoneBook() = default;
     //this one exists for demonstration
-    PhoneBook(std::map<std::string, std::string> person_Phone)
+    PhoneBook(const std::multimap<std::string, std::string>& person_Phone)
     {
-        for(auto [person, phone]: person_Phone) this->person_Phone[person]=phone;
+        for(auto [person, phone]: person_Phone)
+            this->person_Phone.insert(std::make_pair(person,phone));
     };
     //this one suggests having a file storing phone numbers and extracting em for further use
     PhoneBook(std::istream& in)
@@ -28,67 +28,67 @@ public:
             std::string person, phone;
             std::getline(in,person,'+');
             std::getline(in,phone);
-            phone = "+" + phone;
-            if(person != "") person_Phone[person] = phone;
+            if(!person.empty()) person_Phone.insert(std::make_pair(person,"+" + phone));
         }
     }
 
     ~PhoneBook() = default;
 
     //func that returns a sorted list
-    std::map<std::string,std::string> SortedList()
+    std::multimap<std::string,std::string> SortedList()
     {
         return person_Phone;
     }
 
     //func that allows to get a number by the name, looks for exact identity, otherwise returns pair
     //nothing was found
-    std::pair<std::string, std::string> FindUser(std::string user)
+    std::multimap<std::string, std::string> FindUser(std::string user)
     {
-        return person_Phone[user] != ""? std::make_pair(user, person_Phone[user]):
-        std::make_pair("nothing","was found");
+        auto[left, right] = person_Phone.equal_range(user);
+        std::multimap<std::string, std::string> searchRequest;
+        for(auto it = left; it != right; it++)
+            searchRequest.insert(std::make_pair(it->first, it->second));
+        return searchRequest;
     }
 
     //func that generates random Name and grants access to it uses previous funk
     std::pair<std::string, std::string> GetRandomUser()
     {
         int n = std::experimental::randint(0, (int)person_Phone.size());
-        std::map<std::string, std::string>::iterator out = person_Phone.begin();
+        auto out = person_Phone.begin();
         std::advance(out,n);
-        return FindUser(out->first);
+        return std::make_pair(out->first, out->second);
     }
 
     void AddPhoneNumber(std::string person, std::string phone)
     {
-        person_Phone[person] = phone;
+        person_Phone.insert(std::make_pair(person, phone));
     }
 
     friend std::ostream& operator <<(std::ostream& out,
-            const PhoneBook& outPhoneBook)
+                                     const PhoneBook& outPhoneBook)
     {
         for(auto [person, phone]: outPhoneBook.person_Phone) out<<person<<": "<<phone<<"\n";
         return out;
     }
 };
 
-
-
 int main()
 {
-    std::map<std::string, std::string> phoneBook;//first name, second number
-    phoneBook["Gregory McMilan"] = "+1 268 567 0890";
-    phoneBook["Jesus Gomez"] = "+54 567 6540";
-    phoneBook["Niclas Michelson"] = "+47 457 8463";
+    std::multimap<std::string, std::string> phoneBook;//first name, second number
+    phoneBook.insert(std::make_pair("Gregory McMilan", "+1 268 567 0890"));
+    phoneBook.insert(std::make_pair("Jesus Gomez", "+54 567 6540"));
+    phoneBook.insert(std::make_pair("Niclas Michelson", "+47 457 8463"));
+
 
     PhoneBook myPhoneBook(phoneBook);
     myPhoneBook.AddPhoneNumber("Jeniffer Chang", "+81 912 4563");
-    std::map<std::string, std::string> SortedList = myPhoneBook.SortedList();
+    myPhoneBook.AddPhoneNumber("Jeniffer Chang", "+44 928 2020");
+    std::multimap<std::string, std::string> SortedList = myPhoneBook.SortedList();
     std::cout << myPhoneBook;
-    std::cout<<"\n"<<myPhoneBook.FindUser("Jeniffer Chang").first<<" "
-            <<myPhoneBook.FindUser("Jeniffer Chang").second<<"\n";
+    std::multimap<std::string,std::string> searchResult(myPhoneBook.FindUser("Jeniffer Chang"));
 
-    std::cout<<"\n"<<myPhoneBook.FindUser("Jenifer Chang").first<<" "
-             <<myPhoneBook.FindUser("Jenifer Chang").second<<"\n";
+    for(auto [user, phone]: searchResult) std::cout<<user<<": "<<phone;
 
     std::ifstream phoneBookFile;
     phoneBookFile.open("PhoneBook.txt");
@@ -96,7 +96,6 @@ int main()
     phoneBookFile.close();
 
     std::cout<<"\n"<<myFilePhoneBook;
+
     return 0;
-
-
 }
